4cde6b56f1d4e8f8f08dec82ca335fdc55bab38d
mobogemuichi-creator <mobogemuichi@gmail.com>
2026-08-16 10:16:15 +0800

    Fitur auto clone jika folder .git belum ada untuk pengguna awam

--- 3_run_bot_emulator_meter2.bat
+++ 3_run_bot_emulator_meter2.bat
@@ -15,9 +15,29 @@ echo ========================================================
 echo            MENJALANKAN BOT EMULATOR METER
 echo ========================================================
 echo.
-echo [1/2] Memeriksa & mengunduh pembaruan dari GitHub...
-git pull origin main
-echo.
+
+where git >nul 2>&1
+if %errorLevel% neq 0 (
+    echo [PERINGATAN] Git belum ter-install di komputer ini.
+    echo Harap install Git dari https://git-scm.com/ agar bisa auto-update.
+    echo.
+) else (
+    if not exist ".git" (
+        echo [1/2] Pertama kali dijalankan! Mengunduh seluruh project dari GitHub...
+        git init
+        git remote add origin https://github.com/mobogemuichi-creator/bot_fasih.git
+        git fetch origin main
+        git reset --hard origin/main
+        git branch -M main
+        git branch --set-upstream-to=origin/main main
+        echo.
+    ) else (
+        echo [1/2] Memeriksa & mengunduh pembaruan dari GitHub...
+        git pull origin main
+        echo.
+    )
+)
+
 echo [2/2] Memulai bot emulator meter...
 echo.
 
